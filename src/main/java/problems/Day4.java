b391ccd0e4fcf05b3d9629d083a84f283900fa5d
Akash Sharma S <present.akash@gmail.com>
2017-12-18 22:35:38 +0530

    Solved day 4 part 2.

--- src/main/java/problems/Day4.java
+++ src/main/java/problems/Day4.java
@@ -4,9 +4,7 @@ import util.InputUtil;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by akash on 18/12/17.
@@ -16,6 +14,14 @@ public class Day4 {
         int validCount = 0;
         for (String passphrase: passphrases) {
             if (isValid(passphrase)) validCount++;
+        }
+        return validCount;
+    }
+
+    public int getNumOfValidPassphrasesPart2(List<String> passphrases) {
+        int validCount = 0;
+        for (String passphrase: passphrases) {
+            if (isValidPart2(passphrase)) validCount++;
         }
         return validCount;
     }
@@ -30,13 +36,49 @@ public class Day4 {
         return true;
     }
 
+    public boolean isValidPart2(String passphrase) {
+        if (!isValid(passphrase)) return false;
+
+        String[] tokens = passphrase.split("\\s+");
+        for (int i = 0; i < tokens.length - 1; i++) {
+            for (int j = i + 1; j < tokens.length; j++) {
+                if (isAnagram(tokens[i], tokens[j])) return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isAnagram(String input1, String input2) {
+        if (input1.length() != input2.length()) return false;
+
+        Map<Character, Integer> charMap = new HashMap<>();
+        for (char c: input1.toCharArray()) {
+            if (charMap.containsKey(c)) {
+                charMap.put(c, charMap.get(c) + 1);
+            } else {
+                charMap.put(c, 1);
+            }
+        }
+
+        for (char c: input2.toCharArray()) {
+            if (charMap.containsKey(c)) {
+                int count = charMap.get(c);
+                if (count <= 0) return false;   // Not an anagram.
+                charMap.put(c, count - 1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Day4 day4 = new Day4();
         InputUtil inputUtil = new InputUtil();
         String inputPath = day4.getClass().getResource(File.separator + "Day4Input.txt").getFile();
         try {
             List<String> inputLines = inputUtil.getInputLines(inputPath);
-            System.out.println(day4.getNumOfValidPassphrases(inputLines));
+            System.out.println(day4.getNumOfValidPassphrasesPart2(inputLines));
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/test/java/problems/Day4Test.java
+++ src/test/java/problems/Day4Test.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 public class Day4Test {
     Day4 day4;
     String[] passphrases = {"aa bb cc dd ee", "aa bb cc dd aa", "aa bb cc dd aaa"};
+    String[] passphrases2 = {"abcde fghij", "abcde xyz ecdab", "a ab abc abd abf abj", "iiii oiii ooii oooi oooo", "oiii ioii iioi iiio"};
 
     @BeforeMethod
     public void setUp() throws Exception {
@@ -28,6 +29,26 @@ public class Day4Test {
         Assert.assertTrue(day4.isValid(passphrases[0]));
         Assert.assertFalse(day4.isValid(passphrases[1]));
         Assert.assertTrue(day4.isValid(passphrases[2]));
+    }
+
+    @Test
+    public void testGetNumOfValidPassphrasesPart2() throws Exception {
+        Assert.assertEquals(day4.getNumOfValidPassphrasesPart2(Arrays.asList(passphrases2)), 3);
+    }
+
+    @Test
+    public void testIsValidPart2() throws Exception {
+        Assert.assertTrue(day4.isValidPart2(passphrases2[0]));
+        Assert.assertFalse(day4.isValidPart2(passphrases2[1]));
+        Assert.assertTrue(day4.isValidPart2(passphrases2[2]));
+        Assert.assertTrue(day4.isValidPart2(passphrases2[3]));
+        Assert.assertFalse(day4.isValidPart2(passphrases2[4]));
+    }
+
+    @Test
+    public void testIsAnagram() throws Exception {
+        Assert.assertTrue(day4.isAnagram("abcde", "ecdab"));
+        Assert.assertTrue(day4.isAnagram("oiii", "ioii"));
     }
 
 }
